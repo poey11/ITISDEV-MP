1dc02fcd976a028c429ec74dd6476844bbdd2004
poey11 <mocpayao15@gmail.com>
2024-08-03 08:47:05 +0800

    Update feedbackController.js

--- itisdev-mp/backend/controllers/feedbackController.js
+++ itisdev-mp/backend/controllers/feedbackController.js
@@ -4,7 +4,6 @@ const recordFeedback = async(req, res) => {
     try {
         const {reservationId, feedbackData } = req.body;
         
-        console.log(reservationId, feedbackData);
         const feedbackRecord = await feedback.create({reservationId,questions:feedbackData});
         res.status(201).json(feedbackRecord);
     } catch (error) {
