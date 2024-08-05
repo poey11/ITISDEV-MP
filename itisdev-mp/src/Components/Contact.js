18982dda1d9afc795bd91cdfd71bff8d4e86fe15
Tofu <66412194+UnusualCoder26@users.noreply.github.com>
2024-08-05 18:19:52 +0800

    Update Contact.js

--- itisdev-mp/src/Components/Contact.js
+++ itisdev-mp/src/Components/Contact.js
@@ -1,12 +1,11 @@
 import "../Styling/styles.css";
 
-
-const Contact = ({onClose}) => {
+const Contact = ({ onClose }) => {
     return ( 
         <div id="contact-popup" className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="popup-container relative p-8 rounded-lg" style="background-color: #333; color: #fff;">
-                <button style={{paddingRight: '15px', color: '#994a1d'}} className="absolute top-2 right-2 text-white" onClick={onClose}>X</button>
-                <h2 style={{fontWeight: 'bolder'}} className="text-2xl mb-4">Contact Us</h2>
+            <div className="popup-container relative p-8 rounded-lg" style={{ backgroundColor: '#333', color: '#fff' }}>
+                <button style={{ paddingRight: '15px', color: '#994a1d' }} className="absolute top-2 right-2 text-white" onClick={onClose}>X</button>
+                <h2 style={{ fontWeight: 'bolder' }} className="text-2xl mb-4">Contact Us</h2>
                 <p>For inquiries or reservations, please reach out to us:</p>
                 <p><strong>Address:</strong> Hotel Manila, Manila, PH</p>
                 <p><strong>Phone:</strong> +63 9120328432</p>
@@ -15,5 +14,5 @@ const Contact = ({onClose}) => {
         </div>    
     );
 }
-    
+
 export default Contact;
