857acaf502c5afebea0eb14c3db3de8d681f8f59
Tofu <66412194+UnusualCoder26@users.noreply.github.com>
2024-08-05 18:03:43 +0800

    Update Contact.js

--- itisdev-mp/src/Components/Contact.js
+++ itisdev-mp/src/Components/Contact.js
@@ -4,7 +4,7 @@ import "../Styling/styles.css";
 const Contact = ({onClose}) => {
     return ( 
         <div id="contact-popup" className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="popup-container relative bg-white p-8 rounded-lg">
+            <div className="popup-container relative p-8 rounded-lg" style="background-color: #333; color: #fff;">
                 <button style={{paddingRight: '15px', color: '#994a1d'}} className="absolute top-2 right-2 text-white" onClick={onClose}>X</button>
                 <h2 style={{fontWeight: 'bolder'}} className="text-2xl mb-4">Contact Us</h2>
                 <p>For inquiries or reservations, please reach out to us:</p>
